36af25d618d1057a779939c0cb3147d4c476deec
Viktor Syrotkin <vsvs2262asdfg@gmail.com>
2020-02-12 09:51:34 +0300

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Viktor Syrotkin, TI-92'
+
+'if __name__ == '__main__':
+        app.run('0.0.0.0')
