d2e6cf9ec39623a99d0462b93e1cbca608e53e76
Viktor Syrotkin <vsvs2262asdfg@gmail.com>
2020-02-12 09:53:49 +0300

    initial

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def hello():
     name = request.args.get("name", "World")
     return f'Viktor Syrotkin, TI-92'
 
-'if __name__ == '__main__':
-        app.run('0.0.0.0')
+if __name__ == '__main__':
+       app.run('0.0.0.0')
